0b6fbbe4b369aea4578864b797fff77ee0a735d3
nikitha11 <nshankaregowda@qwinix.io>
2018-04-03 09:54:37 +0530

    all

--- polymorphism.rb
+++ polymorphism.rb
@@ -2,33 +2,35 @@ class Duck
 
 	def speak(spea)
 		spea.speak
+		puts "duck can speak"
 	end
 	def fly(fl)
 		fl.fly
+		puts "duck can fly"
 	end
 end
 class Penguin
 
 		def speak
-				puts "dfdkjfh"
+				puts "penguin speaks"
 		end
 		def fly
-				puts "fly"
+				puts "penguin can fly"
 end
 end
 class Bird
 
-		def speak
-			puts "asdjkasdj"
-		end
-		def fly
-		 puts "fl"
-		end
+	def speak
+		puts "birds can speak"
+	end
+	def fly
+		puts "birds can fly"
+	end
 end
 du=Duck.new
 d=Penguin.new
 du.speak(d)
 du.fly(d)
-dd=Bird.new
-du.speak(dd)
-du.fly(dd)
+# dd=Bird.new
+# du.speak(du)
+# du.fly(du)
